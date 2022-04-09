2b9b2b9884d19de4dc55100ccf6611a0d1529e12
Fabio <fkawano@2mundos.net>
2022-04-09 12:21:22 -0300

    hotfix bot.py

--- bot.py
+++ bot.py
@@ -46,7 +46,7 @@ def get_op_page(game_name):
 def get_op_info(url):
     op_content = requests.get(url, headers = headers).text
     url = BeautifulSoup(op_content, 'html.parser')
-    if url.find('strong', {'text': re.compile('percentile')} ):
+    if url.find('strong', {'text': re.compile('percentile')} ) is None:
         rating_text = url.find_all('div', class_ = 'inner-orb')
         rating = rating_text[0].text
         recommendation = rating_text[1].text
