fe7aacfc4a8bc6dde106618faf6fabe117e84de8
Fabio <fkawano@2mundos.net>
2022-05-17 22:57:25 -0300

    new game stuff

--- bot.py
+++ bot.py
@@ -16,6 +16,7 @@ import os
 from bs4 import BeautifulSoup
 from howlongtobeatpy import HowLongToBeat
 from translate import Translator
+from datetime import datetime
 
 t = Translator(to_lang="pt-BR")
 
@@ -27,69 +28,66 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
-key = 'd87fbf5f'
+omdb_api_key = 'd87fbf5f'
 movieExists = False
-TOKEN = '5205421916:AAEBcTQYdpYt6HKmW6VieNrSnibr-5GS6vg'
-
-headers = {
-	'Accept' : '*/*',
-	'Accept-Language': 'en-US,en;q=0.5',
-	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.82',
-}
+BOT_TOKEN = '5205421916:AAEBcTQYdpYt6HKmW6VieNrSnibr-5GS6vg'
 
 
-def get_op_page(game_name):
-    #Make a google search with the game name provided by the user, get parse the results and grab the link of the first valid OpenCritic page.
-    url = 'https://www.google.com/search?q="open+critic"+' + game_name
-    content = requests.get(url, headers = headers).text
-    soup = BeautifulSoup(content, 'html.parser')
-    hasOP = soup.find('h3', text = re.compile('OpenCritic'))
-    if hasOP is not None:
-        firstrating = soup.find('h3', text = re.compile('for'))
-        print(firstrating)
-        if firstrating is None:
-            return 'https://opencritic.com/game/3698/score/reviews'
-        else:
-            first_link = firstrating.find_previous('a')
-            return first_link['href']
-    else: 
-        return 'https://opencritic.com/game/3698/score/reviews'
 
-def get_op_info(url):
-    # Get the OpenCritic page of the game and parse the information
-    op_content = requests.get(url, headers = headers).text
-    url = BeautifulSoup(op_content, 'html.parser')
+igdb_client_id = 'rb28wttfszwg5kki1baracnzlki67z'
+igdb_secret = 'wicf4p0pq1p76nexptfpxdopvp9tx2'
 
-    #if the page is not a valid OpenCritic page, return a message
-    wrong = url.find('strong', text= re.compile('percentile'))
-    if wrong is not None:    
-        return 'Jogo não encontrado no banco de dados do OpenCritic', '', '', 'https://i.imgur.com/2lFiGXm.png', '', '', '', ''
+def get_igdb_game_info(game_name):
+    
+    response = requests.post("https://id.twitch.tv/oauth2/token?client_id="+igdb_client_id+"&client_secret="+igdb_secret+"&grant_type=client_credentials")
 
-    #If the page is a valid OpenCritic page, get the information
-    elif wrong is None:
+    access_token = response.json()['access_token']
+    headers = {
+        'Client-ID': igdb_client_id,
+        'Authorization': 'Bearer ' + access_token
+        }
 
-        rating_text = url.find_all('div', class_ = 'inner-orb')
-        if len(rating_text) > 0:
-            rating = rating_text[0].text
-            recommendation = rating_text[1].text
-            game_title = url.find('h1').text
-            game_image_element = url.find('img', {'alt' : game_title + ' header image'})
-            if game_image_element is not None:
-                game_image = game_image_element.get('src')
-            else:
+    game_info = requests.post("https://api.igdb.com/v4/games/?fields=name,aggregated_rating,rating,first_release_date,genres.name,platforms.name,cover.url&limit=1&search="+game_name+"&where=parent_game=null", headers=headers)
+    game_info = game_info.json()
+    if game_info == []:
+        return 'Game not found', '', '', 'https://i.imgur.com/2lFiGXm.png', '', '', '', ''
+    elif game_info != []:    
+        try:
+            game_title = game_info[0]['name']
+
+            try:
+                game_image = game_info[0]['cover']['url']
+            except:
                 game_image = 'https://i.imgur.com/RgTqosu.jpg'
-            
-            platforms = url.find_all('strong')
-            available_platforms = ''
-            for i in range(len(platforms)):
-                available_platforms += platforms[i].text + ', '
+            try:
+                game_critic_rating = str(round(game_info[0]['aggregated_rating'], 0))[:-2]
+            except:
+                game_critic_rating = 'N/A'
+            try:
+                game_user_rating = str(round(game_info[0]['rating'], 0))[:-2]
+            except:
+                game_user_rating = 'N/A'
+            try:
+                game_release_date = datetime.utcfromtimestamp(game_info[0]['first_release_date']).strftime('%d/%m/%Y')
+            except:
+                game_release_date = 'N/A'
+            game_genres = game_info[0]['genres']
+            game_platforms = game_info[0]['platforms']
+            game_platforms_names = ''
+            game_genres_names = ''
+            for i in range(len(game_genres)):
+                game_genres_names += game_genres[i]['name'] + ', '  
+                
+            for i in range(len(game_platforms)):
+                game_platforms_names += game_platforms[i]['name'] + ', '
 
-            available_platforms = available_platforms[:-2]
+            game_genres_names = game_genres_names[:-2]
+            game_platforms_names = game_platforms_names[:-2]
 
-            #Based on the game name, get HowLongToBeat's information
+            #Based on the game name, get HowLongToBeat's information]
             results_list = HowLongToBeat().search(game_title)
             if results_list is not None and len(results_list) > 0:
-                best_element = max(results_list, key=lambda element: element.similarity)
+                best_element = results_list[0]
                 if best_element.gameplay_main_unit is not None:       
                     hltb_main = (str(best_element.gameplay_main) + " " +best_element.gameplay_main_unit)
                     hltb_main = hltb_main.replace('Hours', 'Horas')
@@ -114,26 +112,36 @@ def get_op_info(url):
                 else: 
                     hltb_completionist = 'N/A'
                 
-                return rating, recommendation, game_title, game_image, available_platforms, hltb_main, hltb_extras, hltb_completionist
+                return game_title, game_image, game_critic_rating, game_user_rating, game_release_date, game_genres_names, game_platforms_names, hltb_main, hltb_extras, hltb_completionist
             else:
 
-                return rating, recommendation, game_title, game_image, available_platforms, '', '', ''
-        else :
-            return 'Jogo não encontrado no banco de dados do OpenCritic', '', '', 'https://i.imgur.com/2lFiGXm.png', '', '', '', ''
+                return game_title, game_image, game_critic_rating, game_user_rating, game_release_date, game_genres_names, game_platforms_names, 'N/A', 'N/A', 'N/A'
+        except:
+            return 'Deu um ruim inesperado', '', '', '', '', '', '', '', '', ''
 
 
-def error(update, context):
-    """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
+def print_igdb_info(update: Update, context: CallbackContext):
+    game_name = " ".join(context.args)
+    game_title, game_image, game_critic_rating, game_user_rating, game_release_date, game_genres_names, game_platforms_names, hltb_main, hltb_extras, hltb_completionist = get_igdb_game_info(game_name)
+    if game_title == 'Game not found' or game_title == 'Deu um ruim inesperado':
+        update.message.reply_photo(game_image, caption= str(game_title))
+
+    else:
+        txt = ('Game: ' + game_title + '\n' + 'Critic Rating: ' + game_critic_rating + '\n' + 'User Rating: ' + game_user_rating + '\n' + 'Platforms: ' + game_platforms_names + '\n' + 'Release Date: ' + game_release_date + '\n' + 'Genres: ' + game_genres_names+ '\n' + 'Time to beat: ' + hltb_main + '\n' + 'Time to beat + extras: ' + hltb_extras + '\n' + 'Time to beat everything: ' + hltb_completionist)
+        update.message.reply_photo(game_image, caption= str(txt))
+
+
 
 
 def start(update: Update, context: CallbackContext):
     update.message.reply_text('fala fiote')
 
 
+#Get Movie Info
+
 def get_rotten_tomatoes_movie_posters(movie_name, movie_year):
     #Get the movie posters from omdb if the movie is found in the database
-    url = 'http://www.omdbapi.com/?t=' + movie_name + '&y='+ movie_year + '&apikey=' + key
+    url = 'http://www.omdbapi.com/?t=' + movie_name + '&y='+ movie_year + '&apikey=' + omdb_api_key
     response = requests.get(url)
     data = json.loads(response.text)
     if data['Response'] == 'True' and data['Poster'] != 'N/A':
@@ -145,7 +153,7 @@ def get_rotten_tomatoes_movie_posters(movie_name, movie_year):
 
 
 def get_rotten_tomatoes_rating(movie_name, movie_year):
-    url = 'http://www.omdbapi.com/?t=' + movie_name + '&y='+ movie_year + '&apikey=' + key
+    url = 'http://www.omdbapi.com/?t=' + movie_name + '&y='+ movie_year + '&apikey=' + omdb_api_key
     response = requests.get(url)
     data = json.loads(response.text)
     rating_source = 'Rotten Tomatoes'
@@ -165,7 +173,7 @@ def get_rotten_tomatoes_rating(movie_name, movie_year):
         for i in range(len(data['Ratings'])):
             if data['Ratings'][i]['Source'] == 'Rotten Tomatoes':
 
-                txt = ('Título: ' + data['Title'] + '\n' + 'Nota no '+rating_source + ': ' + data['Ratings'][i]['Value'] + '\n' + 'Lançado: ' + released + '\n' + 'Diretor: ' + data['Director'] + '\n' +  'País: ' + country + '\n' + 'Box Office: ' + box_office + '\n' +'Sinopse: ||' + plot + '||')
+                txt = ('Título: ' + data['Title'] + '\n' + 'Porcentagem no '+rating_source + ': ' + data['Ratings'][i]['Value'] + '\n' + 'Lançado: ' + released + '\n' + 'Diretor: ' + data['Director'] + '\n' +  'País: ' + country + '\n' + 'Box Office: ' + box_office + '\n' +'Sinopse: ||' + plot + '||')
                 txt_escaped = re.escape(txt)
                 return txt_escaped.replace('\|\|', '||')
         for i in range(len(data['Ratings'])):
@@ -199,30 +207,19 @@ def print_rotten_tomatoes_rating(update: Update, context: CallbackContext):
     txt = get_rotten_tomatoes_rating(movie_name, movie_year)
     update.message.reply_photo(get_rotten_tomatoes_movie_posters(movie_name, movie_year), caption= str(txt), parse_mode="MARKDOWNV2")
 
-def print_op_rating(update: Update, context: CallbackContext):
-    game_name = " ".join(context.args)
-    rating, recommendation, game_title, game_image, available_platforms, hltb_main, hltb_extras, hltb_complete = get_op_info(get_op_page(game_name))
-    if rating == 'Jogo não encontrado no banco de dados do OpenCritic':
-        update.message.reply_photo(game_image, caption= str(rating))
 
-    elif available_platforms != '' and hltb_main != '':
-        available_platforms = available_platforms.replace(', Critic Consensus', '')
-        txt = ('Jogo: ' + game_title + '\n' + 'Média do OpenCritic: ' + rating + '\n' + 'Porcentagem de recomendação da crítica: ' + recommendation + '\n' + 'Plataformas Disponiveis: ' + available_platforms + '\n' + 'Tempo para completar o jogo: ' + hltb_main + '\n' + 'Tempo para completar o jogo com extras: ' + hltb_extras + '\n' + 'Tempo para terminar tudo do jogo: ' + hltb_complete)
-        update.message.reply_photo(game_image, caption= str(txt))
-
-    elif available_platforms != '' and hltb_main == '':
-        available_platforms = available_platforms.replace(', Critic Consensus', '')
-        txt = ('Jogo: ' + game_title + '\n' + 'Média do OpenCritic: ' + rating + '\n' + 'Porcentagem de recomendação da crítica: ' + recommendation + '\n' + 'Plataformas Disponiveis: ' + available_platforms)
-        update.message.reply_photo(game_image, caption= str(txt))
-
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
 
 def pizza(update: Update, context: CallbackContext):
     update.message.reply_photo('https://i.imgur.com/VUEGlFp.jpeg', caption='Pizza!!')
 
 def pudim(update: Update, context: CallbackContext):
     update.message.reply_photo('https://vivareceita-cdn.s3.amazonaws.com/uploads/2021/04/Aprenda-como-fazer-pudim-de-leite-condensado-simples.-Fonte-Brazilian-Kitchen-Abroad.jpg', caption='Pudim!!')
+
 def main():
-    updater = Updater(TOKEN,
+    updater = Updater(BOT_TOKEN,
                   use_context=True)
     updater.dispatcher.add_handler(CommandHandler('pizza', pizza))
     updater.dispatcher.add_handler(CommandHandler('pudim', pudim))
@@ -236,8 +233,8 @@ def main():
 
     updater.start_webhook(listen="0.0.0.0",
                             port=int(PORT),
-                            url_path=TOKEN,
-                            webhook_url='https://supernota-bot.herokuapp.com/' + TOKEN)
+                            url_path=BOT_TOKEN,
+                            webhook_url='https://supernota-bot.herokuapp.com/' + BOT_TOKEN)
 
     updater.idle()
     
