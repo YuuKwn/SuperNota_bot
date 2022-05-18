9b47cfe828c33b3a769a43e8ce15eeb62d842994
Fabio <fkawano@2mundos.net>
2022-05-17 23:06:02 -0300

    please work

--- bot.py
+++ bot.py
@@ -225,7 +225,7 @@ def main():
     updater.dispatcher.add_handler(CommandHandler('pudim', pudim))
     updater.dispatcher.add_handler(CommandHandler('start', start))
     updater.dispatcher.add_handler(CommandHandler('nota', print_rotten_tomatoes_rating))
-    updater.dispatcher.add_handler(CommandHandler('game', print_op_rating))
+    updater.dispatcher.add_handler(CommandHandler('game', print_igdb_info))
     ##updater.dispatcher.add_handler(MessageHandler(Filters.text, echo))
     updater.dispatcher.add_error_handler(error)
 
