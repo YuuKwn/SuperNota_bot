64bba9a89e90ac1d1bf41fb2c05d24dbac920f45
Fabio <fkawano@2mundos.net>
2022-07-20 14:29:33 -0300

    testing custom keyboard

--- bot.py
+++ bot.py
@@ -149,7 +149,7 @@ def get_results(update: Update, context: CallbackContext):
         for i in range(4):
             d["option_{0}".format(i)] = [data['Search'][i]['Title'], data['Search'][i]['imdbID']]
         buttons = [[KeyboardButton(d['option_0'][0])], [KeyboardButton(d['option_1'][0])], [KeyboardButton(d['option_2'][0])], [KeyboardButton(d['option_3'][0])]]
-        context.bot.edit_message_reply_markup(chat_id=update.effective_chat.id, text= 'Pick one', reply_markup=ReplyKeyboardMarkup(buttons, one_time_keyboard=True, selective=True))
+        context.bot.send_message(chat_id=update.effective_chat.id, reply_to_message_id=True ,text= 'Pick one', reply_markup=ReplyKeyboardMarkup(buttons, one_time_keyboard=True, selective=True))
 
     elif len(data['Search']) == 3:
         for i in range(3):
